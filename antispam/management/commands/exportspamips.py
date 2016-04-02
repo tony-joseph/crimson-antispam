2f76e0d2bbc7b2b891d24afea1d0dfed86b52ff2
Tony Joseph <tony.joseph@live.com>
2016-04-02 10:49:28 +0530

    Adds option to import and export spam ip addresses.
    - Adds importspamips management command
    - Adds exportspamips management command

---
+++ antispam/management/commands/exportspamips.py
@@ -0,0 +1,35 @@
+import csv
+import os
+
+from django.core.management.base import BaseCommand
+from django.conf import settings
+from django.db import IntegrityError
+
+from antispam.models import SpamIP
+
+
+class Command(BaseCommand):
+    help = "Exports spam IP address from csv file into database"
+
+    def add_arguments(self, parser):
+        parser.add_argument('csv_files', nargs='+', type=str)
+
+    def handle(self, *args, **kwargs):
+        inserted_count = 0
+        skipped_count = 0
+
+        for csv_file in kwargs['csv_files']:
+            with open(csv_file, 'r') as file_handle:
+                csv_reader = csv.reader(file_handle)
+                for row in csv_reader:
+                    try:
+                        SpamIP.objects.create(
+                            ip_address=row[0],
+                            created_on=row[1],
+                        )
+                        inserted_count += 1
+                    except IntegrityError:
+                        skipped_count += 1
+
+        self.stdout.write(self.style.SUCCESS('Inserted {} new ip addresses'.format(inserted_count)))
+        self.stdout.write(self.style.WARNING('Skipped {} existing addresses'.format(skipped_count)))
---
+++ antispam/management/commands/importspamips.py
@@ -0,0 +1,20 @@
+import csv
+import os
+
+from django.core.management.base import BaseCommand
+from django.conf import settings
+
+from antispam.models import SpamIP
+
+
+class Command(BaseCommand):
+    help = "Imports spam IP address into csv"
+
+    def handle(self, *args, **kwargs):
+        spam_ips = SpamIP.objects.all()
+        file_path = os.path.join(settings.BASE_DIR, 'spamips.csv')
+        with open(file_path, 'w') as csvfile:
+            csvwriter = csv.writer(csvfile)
+            for ip in spam_ips:
+                ip_list = [ip.ip_address, str(ip.created_on)]
+                csvwriter.writerow(ip_list)
